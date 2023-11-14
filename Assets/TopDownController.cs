b6ec76e2de6f58dde3b3e61c7e9f075928b2edf3
krarpit <kumar.arpit77@yahoo.com>
2023-11-13 22:00:26 -0800

    implemented 8-directional movement + walking animations using animator

--- Assets/TopDownController.cs
+++ Assets/TopDownController.cs
@@ -5,21 +5,23 @@ using UnityEngine.InputSystem;
 
 public class TopDownController : MonoBehaviour {
     
-    public float moveSpeed = 1f;
-
+    private float moveSpeed = 2f;  
     private Rigidbody2D body;
     private Animator animator;
     private Vector2 moveInput;
 
-    // Start is called before the first frame update
+    // Purpose: Start is called before the first frame update
     void Start() {
         body = GetComponent<Rigidbody2D>();  // get a reference to the rigid body component of this object
-        animator = GetComponent<Animator>(); // get a reference to the animator controller of this object
+        animator = GetComponent<Animator>(); //         ''           animator controller         ''
     }
-    
+
+    // Purpose: OnMove is called each time a player control key is pressed (look at Assets/Player.inputactions
+    // for list of all key mappings). For movement, these are the arrow and WASD keys.
+    // More about Unity's "new" (it's really been around for a while now) input system here: 
+    // https://gamedevbeginner.com/input-in-unity-made-easy-complete-guide-to-the-new-system/
     public void OnMove(InputValue value) {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
         // only set the animation direction if the player is trying to move
         if (moveInput != Vector2.zero) {
             animator.SetFloat("XInput", moveInput.x);
@@ -27,9 +29,15 @@ public class TopDownController : MonoBehaviour {
         }
     }
 
-    // Update is called once per frame
+    // Purpose: Update is called once per frame
     void Update() {
-        Vector2 moveVector = moveInput * moveSpeed * Time.fixedDeltaTime;
-        body.MovePosition(body.position + moveVector);
+        if (moveInput != Vector2.zero) {
+            Vector2 moveVector = moveInput * moveSpeed * Time.fixedDeltaTime;
+            body.MovePosition(body.position + moveVector);
+            animator.SetBool("isWalking", true);
+        } else {
+            animator.SetBool("isWalking", false);
+        }
+        
     }
 }
