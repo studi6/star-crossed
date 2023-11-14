af4818124bb00d51322e3c960dd8a7a4ed0677a3
krarpit <kumar.arpit77@yahoo.com>
2023-11-13 20:19:18 -0800

    4-directional movement now using animator controller and new input system

--- Assets/TopDownController.cs
+++ Assets/TopDownController.cs
@@ -1,84 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TopDownController : MonoBehaviour {
-    public Rigidbody2D body;
-    public SpriteRenderer spriteRenderer;
+    
+    public float moveSpeed = 1f;
 
-    // sprite lists for different directions 
-    // we reuse lists by flipping them to get 8 total directions
-    // n, ne, e, se, s, sw, w, nw
-    public List<Sprite> nSprites;
-    public List<Sprite> neSprites;
-    public List<Sprite> eSprites;
-    public List<Sprite> seSprites;
-    public List<Sprite> sSprites;
-    public float walkSpeed;
-    public float frameRate;
+    private Rigidbody2D body;
+    private Animator animator;
+    private Vector2 moveInput;
 
-    float idleTime;
-    Vector2 direction;
-    Sprite idleSprite = null;
     // Start is called before the first frame update
     void Start() {
-        // nothing to do here yet...
+        body = GetComponent<Rigidbody2D>();  // get a reference to the rigid body component of this object
+        animator = GetComponent<Animator>(); // get a reference to the animator controller of this object
+    }
+    
+    public void OnMove(InputValue value) {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+        // only set the animation direction if the player is trying to move
+        if (moveInput != Vector2.zero) {
+            animator.SetFloat("XInput", moveInput.x);
+            animator.SetFloat("YInput", moveInput.y);
+        }
     }
 
     // Update is called once per frame
     void Update() {
-        // get direction based on input
-        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
-        // set walk based on direction
-        body.velocity = direction * walkSpeed;
-        handleSpriteFlip();  // mirror current sprite (along y axis) if needed
-        
-        List<Sprite> directionSprites = getSpriteDirection();   // returns lists of all sprites facing curr direction
-        if (directionSprites != null) {
-            float playTime = Time.time - idleTime;              // time since we started walking (in seconds)
-            int totalFrames = (int)(playTime * frameRate);      // total frames since we started
-            int frame =  totalFrames % directionSprites.Count;  // current frame
-            spriteRenderer.sprite = directionSprites[frame];    // set current sprite based on current frame to animate
-            idleSprite = directionSprites[1];                   // record the idle sprite for this direction
-                                                                // we don't want animation to stop while the sprite is walking
-        } else {
-            idleTime = Time.time;
-            if (idleSprite != null) spriteRenderer.sprite = idleSprite;
-        }
-    }
-
-    // Purpose: mirror sprite, this allows us to reuse sprite lists
-    // for eg. all sprites for moving right can be used for moving left by 
-    //         mirroring them along the y-axis
-    void handleSpriteFlip() {
-        if (!spriteRenderer.flipX && direction.x < 0) {
-            spriteRenderer.flipX = true;
-        } else if (spriteRenderer.flipX && direction.x > 0) {
-            spriteRenderer.flipX = false;
-        }
-    }
-
-    // Purpose: return list of all sprite frames for the current direction
-    List<Sprite> getSpriteDirection() {
-        List<Sprite> selectedSprites = null;
-        if (direction.y > 0) { // moving north
-            if (Mathf.Abs(direction.x) > 0) {
-                selectedSprites = neSprites;
-            } else {
-                selectedSprites = nSprites;
-            }
-        } else if (direction.y < 0) { // moving south
-            if (Mathf.Abs(direction.x) > 0) {
-                selectedSprites = seSprites;
-            } else {
-                selectedSprites = sSprites;
-            }
-        } else {  // east/west
-            if (Mathf.Abs(direction.x) > 0) {
-                selectedSprites = eSprites;
-            }
-        }
-
-        return selectedSprites;
+        Vector2 moveVector = moveInput * moveSpeed * Time.fixedDeltaTime;
+        body.MovePosition(body.position + moveVector);
     }
 }
