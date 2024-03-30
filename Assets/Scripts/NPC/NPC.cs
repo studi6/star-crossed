071c2fe489aa080489c19e9a5c874134c4089278
Patticatti <139997703+Patticatti@users.noreply.github.com>
2024-03-30 15:23:45 -0700

    Attached NPC script to kb, interactable w circ collider

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -6,42 +6,33 @@ using UnityEngine.InputSystem;
 public abstract class NPC : MonoBehaviour, IInteractable
 {
     [SerializeField] private SpriteRenderer _interactSprite;
-    private Transform _playerTransform;
-    private const float INTERACT_DISTANCE = 1F;
+    private bool IsWithinInteractDistance;
 
-    private void Start()
-    {
-        _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-    }
-
-    private void Update()
-    {
-        if (Keyboard.current.eKey.wasPressedThisFrame && IsWithinInteractDistance())
-        {
-            //interact when e key is pressed
-            Interact();     
-        }
-
-        if (_interactSprite.gameObject.activeSelf && !IsWithinInteractDistance())
-        {
-            //turn off the sprite
-            _interactSprite.gameObject.SetActive(false);
-        }
-
-        else if (!_interactSprite.gameObject.activeSelf && IsWithinInteractDistance())
-        {
-            //turn on the sprite
-            _interactSprite.gameObject.SetActive(true);
-        }
-    }
     public abstract void Interact();
 
-    private bool IsWithinInteractDistance()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (Vector2.Distance(_playerTransform.position, transform.position) < INTERACT_DISTANCE)
+        if (other.CompareTag("Player"))
         {
-            return true;
+            _interactSprite.gameObject.SetActive(true);
+            IsWithinInteractDistance=true;
         }
-        return false;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (IsWithinInteractDistance && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            Interact();     
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _interactSprite.gameObject.SetActive(false);
+            IsWithinInteractDistance=false;
+        }
     }
 }
